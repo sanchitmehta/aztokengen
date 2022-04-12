7b05998662eeeca04667c0354e1ce4d25fed1fc8
Sanchit Mehta <sanchit.mehta602@gmail.com>
2022-04-12 15:42:24 -0700

    Add token fetcher console cmd

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Simple utility to get oAuth token to interact with Azure APIs
+using Azure.Core;
+using Azure.Identity;
+
+Console.WriteLine("Welcome to AzToken Generator!");
+
+var credentials = new DefaultAzureCredential();
+var token = await credentials.GetTokenAsync(new TokenRequestContext(new string[] { $"https://management.azure.com" }));
+Console.WriteLine($"The Token is '{token.Token}'");
